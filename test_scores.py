03b512cf120c5c458d8f9bcddbe06d9a04f7f939
Johnsmith <57195580+nhojhtims@users.noreply.github.com>
2023-09-23 11:56:47 -0600

    Updated gitignore, andded basic loop

--- test_scores.py
+++ test_scores.py
@@ -1,5 +1,9 @@
 #!/usr/bin/env python3
 
+from cgi import test
+from unittest import TestResult
+
+
 print("The Test Scores Application")
 print()
 
@@ -14,7 +18,21 @@ while more_tests.lower() == "y" :
     score_total = 0
     test_score = 0
     
-    more_tests = input("Enter another set of test scores (y/n)?")
+while (test_score := input("Enter test score: ").lower()) != "end":
+    test_score = int(test_score)
+    if test_score >= 0 and test_score <= 100:
+        score_total += test_score
+        counter += 1
+    else:
+        print("Test score must be between 0 abd 100. Try Again")
+    
+average_score = round(score_total / counter, 2)
+
+print("=========================")
+print(f"Total Score: {score_total}"
+      f"\nAverage Score: {average_score}")
+print()
+more_tests = input("Enter another set of test scores (y/n)?")
     
 
 print("Bye.")
